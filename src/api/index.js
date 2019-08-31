41b7a707d917de4b73dc1e7f54667dbbbc90e377
Luke Elliott <revlucio@gmail.com>
2019-08-31 13:21:37 +0100

    Setup a secret route that requires a password

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const password = process.env.PASSWORD
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/secret', (req, res) => {
+  if (password && req.query.password !== password) {
+    return res.send('Access denied');
+  }
+  return res.send('you found the secret!');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
