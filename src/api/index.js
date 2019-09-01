3b3c40b20ce387628efe5a93b96f9abd157e32cd
Luke Elliott <revlucio@gmail.com>
2019-09-01 10:53:05 +0100

    Add get players endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -21,9 +21,23 @@ app.get('/player', async (req, res) => {
   })
 
   await sequelize.sync()
-  const player = await Player.create({ name: 'Joe' })
+  const player = await Player.create({ name: req.query.name })
 
   return res.send('player created: ' + player.name)
+})
+
+app.get('/players', async (req, res) => {
+  const Sequelize = require('sequelize')
+  const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://:postgres@localhost:5432/darts')
+  
+  const Player = sequelize.define('player', {
+    name: Sequelize.STRING,
+  })
+  
+  await sequelize.sync()
+  const players = await Player.findAll()
+  
+  return res.send(JSON.stringify(players))
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
