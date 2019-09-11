cfbe644ec92a6117695ee6cc452bc60fea141803
Luke Elliott <revlucio@gmail.com>
2019-09-11 21:44:22 +0100

    Style the app using semantic ui

--- src/web/components/Admin.jsx
+++ src/web/components/Admin.jsx
@@ -11,18 +11,20 @@ const addNewPlayer = name =>
     },
   })
 
-const removePlayer = name =>
-  fetch(`/player/${name}`, {
-    method: 'DELETE',
-    headers: {
-      ['Content-Type']: 'application/json; charset=utf-8',
-    },
-  })
-
 const Admin = () => {
   const [isAdmin, setIsAdmin] = useState(null)
   const [newPlayerName, setNewPlayerName] = useState('')
   const [players, setPlayers] = useState([])
+
+  const removePlayer = name =>
+    fetch(`/player/${name}`, {
+      method: 'DELETE',
+      headers: {
+        ['Content-Type']: 'application/json; charset=utf-8',
+      },
+    }).then(() => {
+      setPlayers(players.filter(p => p.name !== name))
+    })
 
   useEffect(() => {
     fetchPlayers(setPlayers)
@@ -38,17 +40,22 @@ const Admin = () => {
         <h2>Admin</h2>
         <fieldset>
           <h3>Add a new player</h3>
-          <label>
-            Name: <input type="text" onChange={e => setNewPlayerName(e.target.value)} />
-          </label>
-          <button onClick={() => addNewPlayer(newPlayerName)}>add</button>
+          <div className="ui input">
+            <input type="text" placeholder="Player name..." onChange={e => setNewPlayerName(e.target.value)} />
+          </div>
+          <button className="ui primary button" onClick={() => addNewPlayer(newPlayerName)}>
+            Add
+          </button>
         </fieldset>
 
         <h2>Players</h2>
         <ul>
           {players.map(player => (
             <li>
-              {player.name} <button onClick={() => removePlayer(player.name)}>remove</button>
+              {player.name}{' '}
+              <button className="ui negative button" onClick={() => removePlayer(player.name, setPlayers)}>
+                Remove
+              </button>
             </li>
           ))}
         </ul>
